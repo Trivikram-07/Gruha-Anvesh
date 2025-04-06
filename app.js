c27c7014f63a7400e1ddfd19f446edcf78996635
Trivikram-07 <sudhakarreddyvikram@gmail.com>
2025-04-06 17:49:49 +0530

    changes-adjusted the order in app.js

--- app.js
+++ app.js
@@ -215,4 +215,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`)); 
